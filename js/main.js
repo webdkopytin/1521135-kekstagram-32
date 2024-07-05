58bf1def53171666a128cc3090f1c74c3718d0a6
Dmitrii Kopytin <web@dkopytin.ru>
2024-07-05 10:27:26 +0300

    fix случайное кол-во комментариев к фото

--- js/main.js
+++ js/main.js
@@ -58,12 +58,8 @@ function createRandomIdFromRangeGenerator (min, max) {
 }
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-const randNumber = Math.round(Math.random() * 30);
-
 const generateIdPhotos = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
 const generateIdUrl = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
-const similarCommentsPhoto = Array.from({length: randNumber}, generateCommentsPhoto);
 
 /**
  * Генерирует комментарии к фотографиям
@@ -78,13 +74,18 @@ function generateCommentsPhoto() {
   };
 }
 
-const createDescriptionPhoto = () => ({
-  id: generateIdPhotos(),
-  url: `photos/${generateIdUrl()}.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: Math.round(Math.random() * 200),
-  comments: similarCommentsPhoto,
-});
+const createDescriptionPhoto = () => {
+  const randNumber = Math.round(Math.random() * 30);
+  const similarCommentsPhoto = Array.from({length: randNumber}, () => generateCommentsPhoto());
+
+  return {
+    id: generateIdPhotos(),
+    url: `photos/${generateIdUrl()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: Math.round(Math.random() * 200),
+    comments: similarCommentsPhoto,
+  };
+};
 
 const similarDescriptionPhoto = Array.from({length: SIMILAR_COMMENTS_COUNT}, createDescriptionPhoto);
 
