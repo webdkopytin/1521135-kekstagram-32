a84c2ad2542d4d1a52835562befa2dec5f985105
Dmitrii Kopytin <web@dkopytin.ru>
2024-07-10 16:22:57 +0300

    fix import

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 import {DESCRIPTIONS, SIMILAR_COMMENTS_COUNT} from './data.js';
 import {getRandomPositiveInteger} from './get-random-positive-integer.js';
 import {createRandomIdFromRangeGenerator} from './create-random-id-from-range-generator.js';
+import {generateCommentsPhoto} from './generate-comments-photo.js';
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 const generateIdPhotos = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
 const generateIdUrl = createRandomIdFromRangeGenerator(1, SIMILAR_COMMENTS_COUNT);
-
-import {generateCommentsPhoto} from './generate-comments-photo.js';
 
 const createDescriptionPhoto = () => {
   const randNumber = Math.round(Math.random() * 30);
